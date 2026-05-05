de6db5a570a700570c5d45e29d47e34679025c4e
GABRIEL MESSIAS DO ESPIRITO SANTO VELOSO DA GLORIA <025.016231@universidadesenaicimatec.edu.br>
2026-05-05 11:24:00 -0300

    att api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://sprint-api-bancaria-fordenter-csharp.onrender.com/api",
+  baseURL: "https://sprint-api-bancaria-fordenter-csharp.onrender.com/",
 });
 
 api.interceptors.request.use((config) => {
