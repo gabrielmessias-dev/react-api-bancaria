e03a607d6294e43db0b504ccede56944f7c24465
GABRIEL MESSIAS DO ESPIRITO SANTO VELOSO DA GLORIA <025.016231@universidadesenaicimatec.edu.br>
2026-05-06 09:51:27 -0300

    ajustando configs para producao

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,15 +1,23 @@
 import axios from "axios";
 
 export const api = axios.create({
+  // /api para facilitar as chamadas nos componentes
   baseURL: "https://sprint-api-bancaria-fordenter-csharp.onrender.com/api",
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    if (token) {
+      // Garanta que o espaço após 'Bearer' existe. 
+      // O uso do .set() é o padrão mais estável nas versões atuais do Axios.
+      config.headers.set('Authorization', `Bearer ${token}`);
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-
-  return config;
-});
+);
