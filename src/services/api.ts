1afdb375258957b962801b8d0199ff554b15989c
GABRIEL MESSIAS DO ESPIRITO SANTO VELOSO DA GLORIA <025.016231@universidadesenaicimatec.edu.br>
2026-05-06 10:58:22 -0300

    configurando para abrir com o back tb

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  // /api para facilitar as chamadas nos componentes
-  baseURL: "https://sprint-api-bancaria-fordenter-csharp.onrender.com/api",
+  // O navegador entende que é "mesmo servidor/api"
+  baseURL: "/api", 
 });
 
 api.interceptors.request.use(
@@ -10,8 +10,6 @@ api.interceptors.request.use(
     const token = localStorage.getItem("token");
 
     if (token) {
-      // Garanta que o espaço após 'Bearer' existe. 
-      // O uso do .set() é o padrão mais estável nas versões atuais do Axios.
       config.headers.set('Authorization', `Bearer ${token}`);
     }
 
